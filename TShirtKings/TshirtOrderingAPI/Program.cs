63d4da558c27f866ae70f2eeadfd25b4e5524877
D-M4N <56065975+D-M4N@users.noreply.github.com>
2019-11-13 13:08:13 +0200

    update

--- TShirtKings/TshirtOrderingAPI/Program.cs
+++ TShirtKings/TshirtOrderingAPI/Program.cs
@@ -1,19 +1,23 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace TshirtOrderingAPI
+using ContosoPets.Api.Data;
+
+using TshirtOrderingAPI;
+
+namespace ContosoPets.Api
+
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            SeedDatabase(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +26,28 @@ namespace TshirtOrderingAPI
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void SeedDatabase(IHost host)
+        {
+            var scopeFactory = host.Services.GetRequiredService<IServiceScopeFactory>();
+
+            using (var scope = scopeFactory.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<ContosoPetsContext>();
+
+                if (context.Database.EnsureCreated())
+                {
+                    try
+                    {
+                        SeedData.Initialize(context);
+                    }
+                    catch (Exception ex)
+                    {
+                        var logger = scope.ServiceProvider.GetRequiredService<ILogger<Program>>();
+                        logger.LogError(ex, "A database seeding error occurred.");
+                    }
+                }
+            }
+        }
     }
 }
--- TShirtKings/TshirtOrderingAPI/Startup.cs
+++ TShirtKings/TshirtOrderingAPI/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using ContosoPets.Api.Data;
 
 namespace TshirtOrderingAPI
 {
@@ -25,6 +27,9 @@ namespace TshirtOrderingAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ContosoPetsContext>(options =>
+           options.UseInMemoryDatabase("ContosoPets"));
+
             services.AddControllers();
         }
 
