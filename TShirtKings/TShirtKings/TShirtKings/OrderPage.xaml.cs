f3e1f3106b2d12da3be3788d9da9719e5d30cde2
D-M4N <56065975+D-M4N@users.noreply.github.com>
2019-11-13 10:50:53 +0200

    update

---
+++ TShirtKings/TShirtKings/TShirtKings/OrderPage.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+using SQLitePCL;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TShirtKings
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class OrderPage : ContentPage
+    {
+        public OrderPage()
+        {
+            InitializeComponent();
+
+        }
+
+
+          
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            var tShirttable = new TShirtTable();
+
+            BindingContext = tShirttable;
+        }
+
+        private async void OnSaveClicked(object sender, EventArgs e)
+        {
+            var tShirttable = (TShirtTable)BindingContext;
+
+            await App.Database.SaveItemAsync(tShirttable);
+            await Navigation.PopAsync();
+
+        }
+
+        private void OnDeleteClicked(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void Cancel_Button(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new ContentsPage());
+        }
+
+
+    }
+}
