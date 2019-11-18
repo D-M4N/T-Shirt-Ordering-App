7824fb988fc8c6ad374ba5d04250ea5df254f81c
D-M4N <56065975+D-M4N@users.noreply.github.com>
2019-11-18 11:59:57 +0200

    update

--- TShirtKings/TShirtKings/TShirtKings/OrderPage.xaml.cs
+++ TShirtKings/TShirtKings/TShirtKings/OrderPage.xaml.cs
@@ -34,52 +34,17 @@ namespace TShirtKings
             BindingContext = tShirttable;
         }
 
-        private async void OnSaveClicked(object sender, EventArgs e)
-        {
-            var url = "http://10.0.2.2:5000/TshirtOrder";
-            var client = new HttpClient();
-            var Product = new 
-            {
-                Name = "D-M4N",
-
-                Gender = "MaN",
-
-                ShirtSize = "XXL",
-
-                DateOfOrder = "Today",
-
-                ShirtName = "CRYTEK",
-
-                ShippingAddress = "MY HOOD",
-
-                EmailAddress = "Just@work.com",
-
-                ContactDetails = "021187911"
-            };
-
-            var json = JsonConvert.SerializeObject(Product);
-            var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
-            try
-            {
-                var response = await client.PostAsync(url, content);
-               // await DisplayAlert("Response Message", response.ReasonPhrase);
-            }
-            catch (Exception ex)
-            {
-                await DisplayAlert("Exception", ex.Message, "Ok");
-            }
-
-
+       
 
             /*
-             *var client = new HttpClient();
+                 *var client = new HttpClient();
 
-              var url = "https://10.0.2.2:5001/TshirtOrder";
+                  var url = "https://10.0.2.2:5001/TshirtOrder";
 
-              var content = new StringContent("{\"name\":\"bob\", \"price\":300}");
+                  var content = new StringContent("{\"name\":\"bob\", \"price\":300}");
 
-              var response = await client.PostAsync(url, content);
-              */
+                  var response = await client.PostAsync(url, content);
+            
 
 
 
@@ -88,24 +53,22 @@ namespace TShirtKings
 
         }
 
-    /*
+    */
      
         private async void OnSaveClicked(object sender, EventArgs e)
         {
             var tShirttable = (TShirtTable)BindingContext;
-
             await App.Database.SaveItemAsync(tShirttable);
-            await Navigation.PopAsync();
 
         }
 
 
-    */
+    
 
 
-        private void OnDeleteClicked(object sender, EventArgs e)
+        private void OnViewOrderClicked(object sender, EventArgs e)
         {
-
+            Navigation.PushAsync(new ViewOrders());
         }
 
 
---
+++ TShirtKings/TShirtKings/TShirtKings/ViewOrders.xaml.cs
@@ -0,0 +1,82 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TShirtKings
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ViewOrders : ContentPage
+    {
+        public List<TShirtTable> TShirtOrders { get; set; }
+        public ViewOrders()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            var tshirtDatabase = App.Database;
+            TShirtOrders = await tshirtDatabase.GetItemsAsync();
+            BindingContext = this;
+        }
+
+        private async void OnConfirmOrderClicked(object sender, EventArgs e)
+        {
+            var client = new HttpClient();
+
+            var url = "https://10.0.2.2:5001/TshirtOrder";
+
+            var content = new StringContent("{\"name\":\"bob\", \"price\":300}");
+
+            var response = await client.PostAsync(url, content);
+
+            /*
+                var tShirttable = (TShirtTable)BindingContext;
+                await App.Database.SaveItemAsync(tShirttable);
+
+
+                var url = "http://10.0.2.2:5000/TshirtOrder";
+                var client = new HttpClient();
+                var Product = new
+                {
+                    Name = "D-M4N",
+
+                    Gender = "MaN",
+
+                    ShirtSize = "XXL",
+
+                    DateOfOrder = "Today",
+
+                    ShirtName = "CRYTEK",
+
+                    ShippingAddress = "MY HOOD",
+
+                    EmailAddress = "Just@work.com",
+
+                    ContactDetails = "021187911"
+                };
+
+                var json = JsonConvert.SerializeObject(Product);
+                var content = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+                try
+                {
+                    var response = await client.PostAsync(url, content);
+                    //await DisplayAlert("Response Message", response.ReasonPhrase);
+                }
+                catch (Exception ex)
+                {
+                    await DisplayAlert("Exception", ex.Message, "Ok");
+                }
+            */
+
+        }
+    }
+}
